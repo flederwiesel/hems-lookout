13b465824c3838504a501a4102931daa44231964
flederwiesel <flederwiesel@fra-flugplan.de>
2023-09-06 23:40:37 +0200

    Cleanup code:
    - Remove unused imports
    - Rename identifiers
    - Add encoding when opening files
    - Reformat code

--- notify.py
+++ notify.py
@@ -4,32 +4,31 @@ import argparse
 import sys
 import os
 import json
-import math
 import re
 import subprocess
-from dataclasses import dataclass
 from gcmath import (
     LatLon,
     calc_bearing,
     calc_distance,
 )
 
-TRACK_DEVIATION = 5 # degrees
-MAX_DISTANCE = 70 # km
+TRACK_DEVIATION = 5  # degrees
+MAX_DISTANCE = 70  # km
 
-def getUserSettings(path):
+
+def get_user_settings(path):
     try:
-        with open(path, "r") as file:
-            return json.load(file)
+        with open(path, "r", encoding="utf-8") as settings:
+            return json.load(settings)
     except FileNotFoundError:
-        pass # for now...
-    except Exception as e:
-        print(e, file=sys.stderr)
+        pass  # for now...
+    except Exception as ex:
+        print(ex, file=sys.stderr)
 
-def formatFlightParams(alt: float,
-                       vrate: float,
-                       distance: float,
-                       speed: float):
+    return []
+
+
+def format_flight_params(alt: float, vrate: float, distance: float, speed: float):
     if int == type(alt):
         result = f"alt {float(alt) * 0.3048:.0f}m"
     elif float == type(alt):
@@ -56,72 +55,98 @@ def formatFlightParams(alt: float,
     if float == type(speed) and speed:
         speed = speed * 1.852
         eta = distance / speed * 60
-        min = int(eta)
-        eta -= min
+        mins = int(eta)
+        eta -= mins
         eta *= 60
-        sec = int(eta)
-        eta = f"eta {min:02d}:{sec:02d} min"
+        secs = int(eta)
+        eta = f"eta {mins:02d}:{secs:02d} min"
         result += f", speed {speed:.0f}km/h eta {eta}"
 
     return result
 
+
 if __name__ == "__main__":
-    user_settings = getUserSettings(os.path.dirname(__file__) + "/notify.json")
+    user_settings = get_user_settings(os.path.dirname(__file__) + "/notify.json")
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-c", "--stdout", action='store_true')
+    parser.add_argument("-c", "--stdout", action="store_true")
 
     args, leftover = parser.parse_known_args()
 
-    for file in leftover:
+    for filename in leftover:
         try:
-            with open(file, 'r') as f:
-                j = json.load(f)
+            with open(filename, "r", encoding="utf-8") as file:
+                adsb = json.load(file)
 
-                for s in j["states"]:
-                    icao, callsign, reg, squawk, lat, lon, alt, vrate, track, speed = s
+                for state in adsb["states"]:
+                    (
+                        icao,
+                        callsign,
+                        reg,
+                        squawk,
+                        lat,
+                        lon,
+                        alt,
+                        vrate,
+                        track,
+                        speed,
+                    ) = state
 
                     if track is not None:
                         if alt is None:
                             alt = "unknown"
 
                         if alt != "ground":
-
                             for user in user_settings:
                                 receipient, locations = user["phone"], user["locations"]
 
-                                for l in locations:
-                                    location, pos = l["name"], LatLon(l["lat"], l["lon"])
+                                for loc in locations:
+                                    location, pos = loc["name"], LatLon(
+                                        loc["lat"], loc["lon"]
+                                    )
 
-                                    b = calc_bearing(LatLon(lat, lon), pos)
+                                    bearing = calc_bearing(LatLon(lat, lon), pos)
 
-                                    if abs(b - track) <= TRACK_DEVIATION:
-                                        d = calc_distance(LatLon(lat, lon), pos)
+                                    if abs(bearing - track) <= TRACK_DEVIATION:
+                                        dist = calc_distance(LatLon(lat, lon), pos)
 
-                                        if d < MAX_DISTANCE:
-                                            params = formatFlightParams(alt, vrate, d, speed)
+                                        if dist < MAX_DISTANCE:
+                                            params = format_flight_params(
+                                                alt, vrate, dist, speed
+                                            )
 
-                                            callsign = re.sub(" *$", " ", callsign) if callsign else ""
+                                            callsign = (
+                                                re.sub(" *$", " ", callsign)
+                                                if callsign
+                                                else ""
+                                            )
 
-                                            message = f"{callsign}{reg}\n" \
-                                                f"{location}\n" \
-                                                f"{params}\n" \
+                                            message = (
+                                                f"{callsign}{reg}\n"
+                                                f"{location}\n"
+                                                f"{params}\n"
                                                 f"https://globe.adsbexchange.com/?icao={icao}"
+                                            )
 
                                             if args.stdout:
                                                 print(f"{message}\n")
                                             else:
-                                                subprocess.call([
-                                                        "signal-cli", "send", "-m", message, receipient
+                                                subprocess.call(
+                                                    [
+                                                        "signal-cli",
+                                                        "send",
+                                                        "-m",
+                                                        message,
+                                                        receipient,
                                                     ],
                                                     stdout=subprocess.DEVNULL,
-                                                    timeout=15
+                                                    timeout=15,
                                                 )
-        except FileNotFoundError as e:
-            print(e, file=sys.stderr)
-        except Exception as e:
-            print(e, file=sys.stderr)
+        except FileNotFoundError as exception:
+            print(exception, file=sys.stderr)
+        except Exception as exception:
+            print(exception, file=sys.stderr)
 
-            if j:
-                print(j, file=sys.stderr)
+            if adsb:
+                print(adsb, file=sys.stderr)
