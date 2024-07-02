d702c5e0ccf5a99e98a5f327f2108c559a3a95e5
flederwiesel <flederwiesel@fra-flugplan.de>
2024-07-02 22:33:56 +0200

    Deploy "gcmath.py".

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ readonly SCRIPTDIR=$(dirname "${BASH_SOURCE[0]}")
 ssh flugplan@fra-flugplan.de mkdir -p hems-lookout
 
 rsync -av "$SCRIPTDIR/" flugplan@fra-flugplan.de:hems-lookout \
+	--filter='+ gcmath.py' \
 	--filter='+ install.sh' \
 	--filter='+ notify.py' \
 	--filter='+ query-adsb.sh' \
